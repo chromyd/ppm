107251cd06768f09194e398f5a56b2803609ff4c
chromyd <dusan.chromy@medicalcolumbus.de>
2014-05-31 01:03:56 +0200

    Rename application ID. Add jersey. Add first successful flickr call.

--- ppm-war/src/main/java/de/blogspot/soahowto/ppm/PhotosetServlet.java
+++ ppm-war/src/main/java/de/blogspot/soahowto/ppm/PhotosetServlet.java
@@ -3,6 +3,8 @@ package de.blogspot.soahowto.ppm;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -21,6 +23,15 @@ public class PhotosetServlet extends HttpServlet {
 	    Properties p = new Properties();
 	    p.load(getClass().getResourceAsStream("/photoset.properties"));
 	    resp.getWriter().println("Token starts with " + p.getProperty("auth_token").substring(0,3));
+	    Client client = ClientBuilder.newClient();
+	    String response = client.target("https://api.flickr.com/services/rest/")
+			    .queryParam("auth_token", p.getProperty("auth_token"))
+			    .queryParam("api_key", p.getProperty("api_key"))
+			    .queryParam("method", "flickr.photos.getWithoutGeoData")
+			    .queryParam("page", 12)
+			    .queryParam("api_sig", "fb5c9d5d4f8e82d8e51ac00a29bac55d")
+			    .request().get(String.class);
+	    resp.getWriter().println("Response:\n" + response);
     }
   }
 }
